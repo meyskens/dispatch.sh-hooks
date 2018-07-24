953d5a1e0673919eec4c84a20d8a4998eefc3883
Maartje Eyskens <meyskens@me.com>
2018-07-24 13:35:26 +0200

    Connect push event to helmet

--- server.js
+++ server.js
@@ -1,28 +1,41 @@
 import express from "express"
 import bodyParser from "body-parser"
+import * as apps from "./components/apps"
+import HelmetController from "./components/helmet"
+
+const helmet = new HelmetController(process.env.HELMET_URL, process.env.HELMET_TOKEN)
 
 const app = express()
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use(bodyParser.raw({ type: "application/vnd.docker.distribution.events.v1+json" }))
 
+const wrap = fn => (...args) => fn(...args).catch(args[2]);
+
 app.get("/", (req, res) => {
     res.send("Duspatch.sh hooks server")
 })
 
-app.all("/docker", (req, res) => {
+app.all("/docker", wrap(async(req, res) => {
     if (req.header("Authorization") !== `Bearer ${process.env.DOCKER_TOKEN}`) {
         return res.status(401).send("Invalid token")
     }
 
     const body = JSON.parse(req.body)
     for (let event of body.events) {
-        console.log(event)
+        if (event.action === "push") {
+            const repo = event.target.repository
+            const tag = event.target.tag
+            
+            const appEntry = apps.getAppForRepo(repo)
+            if (appEntry) {
+                appEntry.values.image = `${repo}:${tag}`
+                await helmet.create(appEntry.internalName, appEntry.values)
+            }
+        }
     }
-    
-    console.log(body)
 
     res.send("ok")
-})
+}))
 
 app.listen(80, () => console.log("Listening on port 80"))
