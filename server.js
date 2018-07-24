71a83ec43b1d241d62f2efbec4b8f32089de866e
Maartje Eyskens <meyskens@me.com>
2018-07-24 09:38:43 +0000

    Add raw body parser

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ import bodyParser from "body-parser"
 const app = express()
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
+app.use(bodyParser.raw())
 
 app.get("/", (req, res) => {
     res.send("Duspatch.sh hooks server")
