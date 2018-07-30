c828d60d3394b79fd7df38f703b02df4fa2e1d11
Maartje Eyskens <meyskens@me.com>
2018-07-30 12:35:16 +0200

    fix read from undefined error

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ app.all("/docker", wrap(async(req, res) => {
             const appEntry = apps.getAppForRepo(repo)
             if (appEntry) {
                 let oldTag = ""
-                if (appEntry.values.image) {
+                if ((appEntry.values | {}).image) {
                     const imageParts = appEntry.values.image.split(":")
                     if (imageParts.length === 2) {
                         oldTag = imageParts[1]
